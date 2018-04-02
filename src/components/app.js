19c78ffb3e925457932e56f3c451a7dcad7fbe88
Glatz <zsglatz@gmail.com>
2018-04-02 15:56:29 -0600

    added our generate button and setState methods

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Birthday Counter</h1>
+        <BirthdayForm />
       </div>
     );
   }
---
+++ src/components/birthdayForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+class BirthdayForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleGenerate = this.handleGenerate.bind(this);
+
+        this.state = {
+            startDate: moment(),
+            formCompleted: false
+
+        }
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        })
+    }
+
+    handleGenerate() {
+        this.setState({
+            formCompleted: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Birthday Form Component </h1>
+                <DatePicker 
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+                <a onClick={this.handleGenerate}> Generate Countdown</a>
+            </div> 
+        )
+    }
+}
+
+export default BirthdayForm;
